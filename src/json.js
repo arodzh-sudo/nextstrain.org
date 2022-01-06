b4bb32b08df79ee079c75d12989df18ca8953379
Thomas Sibley <tsibley@fredhutch.org>
2022-01-05 22:52:57 -0800

    wip! 📙 ✌

---
+++ src/json.js
@@ -0,0 +1,11 @@
+/**
+ * Convert Set and Map objects for {@link JSON.stringify}.
+ */
+const replacer = (key, value) =>
+    value instanceof Set ?                          [...value] :
+    value instanceof Map ? Object.fromEntries(value.entries()) :
+                                                         value ;
+
+module.exports = {
+  replacer,
+};
